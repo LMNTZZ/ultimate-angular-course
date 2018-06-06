28dd53e613dfed5c9cbdfd62f9ef899d3efc31d0
Kevin Bervoets <kevinbervoets@Kevins-MacBook-Pro-4.local>
2018-06-06 10:23:59 +0200

    adding content to template

--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-stock',
-  template: '<h1>Stocks</h1>'
+  selector: 'app-stocks',
+  template: `<h2>Stocks</h2>
+  <h4>{{title}}</h4>
+  <ul>
+  <li *ngFor="let stock of stocks">{{stock}}</li>
+  </ul>
+  `
 })
 
 export class StocksComponent {
-  text = 'my first manual component!';
+  title = 'List of Stocks: ';
+  stocks = ['AAPL', 'IBM', 'GOOG', 'UDEMY'];
 }
