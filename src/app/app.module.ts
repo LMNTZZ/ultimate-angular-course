33369ffa082ea6bf0ae5de21c7a424ddb9a1d9f7
Kevin Bervoets <kevinbervoets@Kevins-MacBook-Pro-4.local>
2018-06-06 10:17:09 +0200

    adding & generating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {StocksComponent} from './stocks.component';
+import { MutualfundsComponent } from './mutualfunds/mutualfunds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StocksComponent,
+    MutualfundsComponent
   ],
   imports: [
     BrowserModule
