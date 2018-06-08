8c21565a423e77bfb6b7d46d55d3d57b9166416c
Kevin Bervoets <kevinbervoets@Kevins-MacBook-Pro-4.local>
2018-06-08 14:21:03 +0200

    compatibility fixes rxjs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StockService} from '../stock.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private stockService: StockService) { }
 
   ngOnInit() {
+    this.getAllStocks();
+  }
+
+  getAllStocks() {
+    this.stockService.getStocksAPI().subscribe(
+      data => console.log(JSON.stringify(data)),
+      error => console.log('Server error')
+    );
   }
 
 }
--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -1,7 +1,22 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StockService {
+
+  constructor(private http : HttpClient) {
+
+  }
+
+  getStocksAPI(): Observable<any>{
+    return this.http.get('http://localhost:3000/stocks')
+      .map( (res: Response) => res.json() )
+      .catch( (error: any) => Observable.throw(error.json().error || 'Server error') );
+  }
+
   getStocks() : string[]{
     return ['AAPL', 'IBM', 'GOOG', 'UDEMY', 'ABC'];
   }
